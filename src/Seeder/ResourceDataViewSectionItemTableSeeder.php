5462edef259747833c22defd86dded3844571bf9
firumon <github@firumon.com>
2018-09-09 20:20:19 +0530

    Data listing with relations implemented

--- src/Seeder/ResourceDataViewSectionItemTableSeeder.php
+++ src/Seeder/ResourceDataViewSectionItemTableSeeder.php
@@ -13,6 +13,17 @@ class ResourceDataViewSectionItemTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $_ = \DB::statement('SELECT @@GLOBAL.foreign_key_checks');
+        \DB::statement('set foreign_key_checks = 0');
+        \Milestone\Appframe\Model\ResourceDataViewSectionItem::truncate()
+            ->create([	'section' => '1', 	'label' => 'Name', 	'attribute' => 'name', 													])
+            ->create([	'section' => '1', 	'label' => 'Email', 	'attribute' => 'email', 													])
+            ->create([	'section' => '1', 	'label' => 'Groups', 	'attribute' => 'title', 	'relation' => '1', 												])
+            ->create([	'section' => '2', 	'label' => 'Name', 	'attribute' => 'name', 													])
+            ->create([	'section' => '2', 	'label' => 'Email', 	'attribute' => 'email', 													])
+            ->create([	'section' => '3', 	'label' => 'Group', 	'attribute' => 'title', 													])
+            ->create([	'section' => '3', 	'label' => 'Description', 	'attribute' => 'description', 													])
+        ;
+        \DB::statement('set foreign_key_checks = ' . $_);
     }
 }
--- src/Seeder/ResourceDataViewSectionTableSeeder.php
+++ src/Seeder/ResourceDataViewSectionTableSeeder.php
@@ -13,6 +13,13 @@ class ResourceDataViewSectionTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $_ = \DB::statement('SELECT @@GLOBAL.foreign_key_checks');
+        \DB::statement('set foreign_key_checks = 0');
+        \Milestone\Appframe\Model\ResourceDataViewSection::truncate()
+            ->create([	'resource_data' => '1', 		'title_field' => 'name', 		'colspan' => '12', 											])
+            ->create([	'resource_data' => '2', 				'colspan' => '4', 											])
+            ->create([	'resource_data' => '2', 	'title' => 'Groups', 		'relation' => '1', 	'colspan' => '8', 											])
+        ;
+        \DB::statement('set foreign_key_checks = ' . $_);
     }
 }
