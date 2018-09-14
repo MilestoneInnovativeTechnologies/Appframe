a8fb75edd05ea83688a744d6d23ac4d9de473611
firumon <github@firumon.com>
2018-09-14 11:58:15 +0530

    DataViewSection renamed to DataDetails

--- src/Controllers/GetDataDetailsController.php
+++ src/Controllers/GetDataDetailsController.php
@@ -4,13 +4,14 @@ namespace Milestone\Appframe\Controllers;
 
 use Milestone\Appframe\Model\ResourceData;
 
-class GetDataViewSectionController extends Controller
+class GetDataDetailsController extends Controller
 {
 
     public function index(){
         $ResDataId = $this->bag->r('data_id');
         $ResData = ResourceData::with(['Sections' => function($Q){ $Q->with(['Relation','Items.Relation']); }])->find($ResDataId);
-        $this->bag->store('DataViewSection',$ResDataId,$ResData->Sections);
+        $this->bag->store('DataDetails',$ResDataId,$ResData);
+        //$this->bag->store('DataViewSection',$ResDataId,$ResData->Sections);
     }
 
 }
