f516d61fa0a3eaac02c53119efd80cce57afdf40
firumon <github@firumon.com>
2018-09-19 13:28:49 +0530

    Helper to fetch deep relations of a record

---
+++ src/Helper/DeepRelationHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Milestone\Appframe\Helper;
+
+use Milestone\Appframe\Model\ResourceRelation;
+
+class DeepRelationHelper
+{
+    private $record, $model;
+
+    public function __construct($record)
+    {
+        $this->record = $record;
+        $this->model = new ResourceRelation;
+    }
+
+    public function get(){
+        return $this->fetchRelations($this->record)->toArray();
+    }
+
+    private function fetchRelations($record){
+        return collect($record)
+            ->only(['relation','nest_relation1','nest_relation2','nest_relation3','nest_relation4','nest_relation5'])
+            ->filter()->map(function($relation_id){
+                return $this->getRelationDetails($relation_id);
+            });
+    }
+
+    private function getRelationDetails($id){
+        return $this->model->find($id);
+    }
+
+}
