d92baf2e036d6e2dce6e754be377bb927f734efb
firumon <github@firumon.com>
2018-09-19 13:01:11 +0530

    Helper/Helper changed to Helper/Help

--- src/Controllers/GetFormController.php
+++ src/Controllers/GetFormController.php
@@ -9,8 +9,8 @@ class GetFormController extends Controller
 
     public function index(){
         $formId = $this->bag->r('form');
-        $form = Helper::Helper('Form',$formId);
-        $validations = Helper::Helper('Validation',$form);
+        $form = Helper::Help('Form',$formId);
+        $validations = Helper::Help('Validation',$form);
         $this->bag->push('Validation',$formId,$validations);
         $this->bag->store('Form',$formId,$form);
     }
