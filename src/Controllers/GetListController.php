a55e39516f026dffad2834df3901c1173e461e4b
firumon <github@firumon.com>
2018-09-19 14:02:00 +0530

    Made use of ListHelper

--- src/Controllers/GetListController.php
+++ src/Controllers/GetListController.php
@@ -2,20 +2,17 @@
 
 namespace Milestone\Appframe\Controllers;
 
-use Illuminate\Support\Str;
-use Milestone\Appframe\Model\ResourceList;
-use Milestone\Appframe\Model\ResourceRelation;
+use Milestone\Appframe\Helper\Helper;
 
 class GetListController extends Controller
 {
 
     public function index(){
         $id = $this->bag->r('list_id');
-        $Data = $this->getORMData($id); $ORM = $this->getListORM($Data);
+        $Data = $this->getORMData($id); dd($Data); $ORM = $this->getListORM($Data);
         $Data = $ORM->get();
         $this->store($id,$Data);
     }
-    private $RelationCache = [];
 
     private function getORMData($id){
         return ($this->hasInSession($id) && $this->bag->r('update') === true)
@@ -32,81 +29,11 @@ class GetListController extends Controller
     }
 
     private function getPreparedORM($id){
-        $Data = ResourceList::with('Resource','Relations','Scopes','Layout')->find($id);
-        $List = $this->getExtractData($Data);
+        $List = Helper::Help('List',$id);
         $this->bag->push('List',$id,$List);
         $this->bag->store('ListData',$id,array_except($List,['orm','layout']));
         $this->bag->store('ListLayout',$id,$List['layout']);
         return $List;
-    }
-
-    private function getExtractData($Data){
-        $items = $Data->items_per_page; $title = $Data->title;
-        $relations = $this->getRelations($Data->Relations);
-        $layout = $this->getLayout($Data->Layout);
-        $last = 0;
-        $orm = $this->getExtractORM($Data);
-        return compact('orm','relations','items','last','title','layout');
-    }
-
-    private function getRelations($Relations){
-        if($Relations->isEmpty()) return null;
-        return $Relations->map(function ($item){
-            $relDeep = collect($item)->only(['relation','nest_relation1','nest_relation2','nest_relation3','nest_relation4','nest_relation5'])->filter()->values();
-            return $this->getRelationDeep($relDeep);
-        })->toArray();
-    }
-
-    private function getLayout($Layout){
-        return ($Layout->isEmpty()) ? ['ID' => 'id'] : $Layout->mapWithKeys(function($layout){
-            return [$layout->label => $this->getLayoutProps($layout)];
-        });
-    }
-
-    private function getLayoutProps($layout){
-        $With = $this->getWithRelations(collect([$layout]))[0];
-        $path = ($With) ? $With = implode(".",array_map(function($rel){ return Str::snake($rel); },explode(".",$With))) : $With;
-        $field = $layout->field;
-        return ['field' => $field, 'path' => $path];
-    }
-
-    private function getExtractORM($Data){
-        $Class = $this->getResourceClass($Data->Resource);
-        $With = $this->getWithRelations($Data->Relations);
-        $Scopes = $this->getScopes($Data->Scopes);
-        return compact('Class','With','Scopes');
-    }
-
-    private function getResourceClass($res){
-        return implode('\\',[$res->namespace,$res->name]);
-    }
-
-    private function getWithRelations($Relations){
-        if($Relations->isEmpty()) return [];
-        $with = [];
-        $Relations->each(function ($item) use(&$with) {
-            $RelDeep = collect($item)->only(['relation','nest_relation1','nest_relation2','nest_relation3','nest_relation4','nest_relation5'])->values()->filter();
-            array_push($with, $this->getRelationDeep($RelDeep));
-        });
-        return $with;
-    }
-
-    private function getScopes($Scopes){
-        if($Scopes->isEmpty()) return null;
-        return $Scopes->map(function($item){
-            return [$item->method,collect($item)->only(['arg1','arg2','arg3'])->filter()->toArray()];
-        });
-    }
-
-    private function getRelationDeep($Coll){
-        $relation = [];
-        $Coll->each(function($item) use(&$relation) {
-            $name = ($this->RelationCache && array_key_exists($item,$this->RelationCache))
-                ? $this->RelationCache[$item]
-                : ResourceRelation::find($item)->method;
-            array_push($relation,$name); $this->RelationCache[$item] = $name;
-        });
-        return implode(".",$relation);
     }
 
     private function getORMFromAttributes($Class,$With,$Scopes){
