d9fada8772309e5cadd4596839619432ee9b67cf
firumon <github@firumon.com>
2018-09-22 13:25:19 +0530

    Convert to plan array only if exists

--- src/Controllers/GetListDetailsController.php
+++ src/Controllers/GetListDetailsController.php
@@ -11,7 +11,8 @@ class GetListDetailsController extends Controller
         $id = $this->bag->r('list_id');
         $List = Helper::Help('List',$id);
 
-        $List['orm']['Scopes'] = $List['orm']['Scopes']->toArray(); $List['layout'] = $List['layout']->toArray();
+        $List['orm']['Scopes'] = $List['orm']['Scopes'] ? $List['orm']['Scopes']->toArray() : null;
+        $List['layout'] = $List['layout'] ? $List['layout']->toArray() : null;
         $this->bag->push('List',$id,$List);
 
         $this->bag->store('ListData',$id,array_except($List,['orm','layout']));
