fb70e13f1ab745b32088d7ee2114b80ba435251d
firumon <github@firumon.com>
2018-09-22 13:09:45 +0530

    Eloquent collections modified as plan array

--- src/Controllers/GetListDetailsController.php
+++ src/Controllers/GetListDetailsController.php
@@ -10,7 +10,10 @@ class GetListDetailsController extends Controller
     public function index(){
         $id = $this->bag->r('list_id');
         $List = Helper::Help('List',$id);
+
+        $List['orm']['Scopes'] = $List['orm']['Scopes']->toArray(); $List['layout'] = $List['layout']->toArray();
         $this->bag->push('List',$id,$List);
+
         $this->bag->store('ListData',$id,array_except($List,['orm','layout']));
         $this->bag->store('ListLayout',$id,$List['layout']);
     }
