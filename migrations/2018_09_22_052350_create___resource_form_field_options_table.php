6fd20fc4f4ad8b3f140d2c7ae5487bc7fd650aa8
firumon <github@firumon.com>
2018-09-22 14:20:00 +0530

    Field options table modified

---
+++ migrations/2018_09_22_052350_create___resource_form_field_options_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResourceFormFieldOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('__resource_form_field_options', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('form_field')->index();
+            $table->string('method', 128)->nullable();
+            $table->unsignedInteger('resource_list')->index();
+            $table->string('value_attr', 64)->default('id');
+            $table->string('label_attr', 128)->nullable();
+            $table->enum('preload', ['Yes','No'])->default('Yes');
+            $table->enum('enum', ['Yes','No'])->default('No');
+            $table->timestamps();
+            $table->foreign('form_field')->references('id')->on('__resource_form_fields')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('resource_list')->references('id')->on('__resource_lists')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('__resource_form_field_options');
+    }
+}
