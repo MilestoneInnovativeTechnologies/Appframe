1c3189c06d3cc5aa85a95731835d6f2d48801552
firumon <github@firumon.com>
2018-09-22 14:18:36 +0530

    Modified to make use of Helper class

--- src/Controllers/GetDataController.php
+++ src/Controllers/GetDataController.php
@@ -2,19 +2,14 @@
 
 namespace Milestone\Appframe\Controllers;
 
-use Milestone\Appframe\Model\Resource;
-use Milestone\Appframe\Model\ResourceData;
+use Milestone\Appframe\Helper\Helper;
 
 class GetDataController extends Controller
 {
 
     public function index(){
         $ResDataId = $this->bag->r('data_id'); $RecId = $this->bag->r('record_id');
-        $ResData = ResourceData::find($ResDataId);
-        $Res = Resource::find($ResData->resource);
-        $Class = implode('\\',[$Res->namespace,$Res->name]);
-        $updatedAt = $this->bag->req('last_updated') ?: 0;
-        $Data = (new $Class)->where('updated_at','>',$updatedAt)->find($RecId);
+        $Data = Helper::Help('GetData',$ResDataId,[ 'record_id' => $RecId ]);
         $this->bag->store('Data',$ResDataId,$Data);
     }
 
