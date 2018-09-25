f942fdeb5840cd8bcc82509fe237b553a6b302a6
firumon <github@firumon.com>
2018-09-25 19:24:56 +0530

    To get list data by giving list id

---
+++ src/Helper/ListDataHelper.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Milestone\Appframe\Helper;
+
+class ListDataHelper
+{
+    private $id;
+    protected $class, $scopes, $with;
+    public $page = 1, $limit = 0, $latest = 0;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+        $list = Helper::Help('List',$id);
+        $this->limit = $list['items'];
+        $this->class = $list['orm']['Class'];
+        $this->scopes = $list['orm']['Scopes'];
+        $this->with = $list['orm']['With'];
+    }
+
+    public function get(){
+        $Model = $this->getModel();
+        return $this->getData($Model,$this->limit);
+    }
+
+    private function getModel(){
+        $Model = new $this->class;
+        $Model = $this->getApplyWith($Model, $this->with);
+        $Model = $this->getApplyScope($Model, $this->scopes);
+        $Model = $this->getApplyLatest($Model, $this->latest);
+        return $Model;
+    }
+
+    private function getApplyWith($Model, $With = []){
+        return ($With && !empty($With))
+            ? $Model->with($With)
+            : $Model;
+    }
+
+    private function getApplyScope($Model, $Scopes = []){
+        if($Scopes && !empty($Scopes)){
+            foreach($Scopes as $Scope){
+                $attrs = $Scope[1]; $method = $Scope[0];
+                $Model = call_user_func_array([$Model, $method], $attrs);
+            }
+        }
+        return $Model;
+    }
+
+    private function getApplyLatest($Model, $latest = 0){
+        return ($latest) ? $Model->where('updated_at','>',$latest) : $Model;
+    }
+
+    private function getData($Model,$Limit){
+        if($Limit){
+            $take = $Limit; $skip = ($this->page-1) * $take;
+            $Model->skip($skip)->take($take);
+        } return $Model->get();
+    }
+
+}
