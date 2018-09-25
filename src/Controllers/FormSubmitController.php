4ce97ae07cd463ae47a87cd2ea47c887e5b36c65
firumon <github@firumon.com>
2018-09-25 11:36:45 +0530

    Made use of Field Relation Extract

--- src/Controllers/FormSubmitController.php
+++ src/Controllers/FormSubmitController.php
@@ -2,6 +2,7 @@
 
 namespace Milestone\Appframe\Controllers;
 
+use Milestone\Appframe\Helper\Helper;
 use Milestone\Appframe\Model\ResourceForm;
 
 class FormSubmitController extends Controller
@@ -14,13 +15,18 @@ class FormSubmitController extends Controller
     }
 
     private function SubmitForm(){
-        $form = $this->bag->r('form_id'); $update = $this->bag->r('update');
-        $Form = ResourceForm::with('Resource','Defaults','Fields.Data')->find($form);
-        $FieldExtractClass = (new Database\FormFieldExtract($Form))->process($update);
-        $RelationGrouped = $FieldExtractClass->relationGrouped;
-        $this->bag->r('submit_relations',$RelationGrouped->keys()->filter()->values()->toArray());
-        $Submit = new Database\Push($RelationGrouped->toArray(),$FieldExtractClass->resourceModel);
+        $form = $this->bag->r('form_id'); $update = $this->bag->r('update'); $data = $this->bag->r('data');
+        $Model = $this->getFormResourceModel($form,$update);
+        $Data = Helper::Help('FormFieldRelationExtract',$form,[ 'input' => $data ]);
+        $this->bag->r('submit_relations',array_filter(array_column($Data,'path')));
+        $Submit = new Database\Push($Data,$Model);
         $this->bag->store('FormSubmit',$form,!!$Submit); $this->bag->r('submit_model',$Submit->Model);
+    }
+
+    private function getFormResourceModel($form,$update){
+        $Res = ResourceForm::find($form)->Resource;
+        $Class = implode("\\",[$Res->namespace,$Res->name]);
+        return ($update) ? (new $Class)->find($update) : new $Class;
     }
 
 }
