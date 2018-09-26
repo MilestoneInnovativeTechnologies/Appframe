58917cc349d8fd54a4c06c2ecd19f41e159d851a
firumon <github@firumon.com>
2018-09-26 21:09:12 +0530

    Filter by Ids implemented

--- src/Helper/ListDataHelper.php
+++ src/Helper/ListDataHelper.php
@@ -6,7 +6,7 @@ class ListDataHelper
 {
     private $id;
     protected $class, $scopes, $with;
-    public $page = 1, $limit = 0, $latest = 0;
+    public $page = 1, $limit = 0, $latest = 0, $ids = [];
 
     public function __construct($id)
     {
@@ -20,7 +20,7 @@ class ListDataHelper
 
     public function get(){
         $Model = $this->getModel();
-        return $this->getData($Model,$this->limit);
+        return $this->getData($Model,$this->limit,(array) $this->ids);
     }
 
     private function getModel(){
@@ -51,11 +51,11 @@ class ListDataHelper
         return ($latest) ? $Model->where('updated_at','>',$latest) : $Model;
     }
 
-    private function getData($Model,$Limit){
+    private function getData($Model,$Limit,$Ids){
         if($Limit){
             $take = $Limit; $skip = ($this->page-1) * $take;
             $Model->skip($skip)->take($take);
-        } return $Model->get();
+        } return ($Ids && !empty($Ids)) ? $Model->find($Ids) : $Model->get();
     }
 
 }
