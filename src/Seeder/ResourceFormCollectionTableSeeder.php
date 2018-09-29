5e3354dd5bbd1a4cc81f322fc4593209eac33f35
firumon <github@firumon.com>
2018-09-29 19:19:51 +0530

    Form Collection Table seeder

--- src/Seeder/ResourceFormCollectionTableSeeder.php
+++ src/Seeder/ResourceFormCollectionTableSeeder.php
@@ -13,6 +13,10 @@ class ResourceFormCollectionTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $_ = \DB::statement('SELECT @@GLOBAL.foreign_key_checks');
+        \DB::statement('set foreign_key_checks = 0');
+        \Milestone\Appframe\Model\ResourceFormCollection::truncate()
+        ;
+        \DB::statement('set foreign_key_checks = ' . $_);
     }
 }
