d79969189cbc30111c1d82c3b42db2da262cfc3e
Firose Hussain <github@firumon.com>
2018-10-06 20:15:56 +0530

    Modified to accept page wise list and id wise list

--- src/Resolve/ListResolver.php
+++ src/Resolve/ListResolver.php
@@ -5,7 +5,11 @@ namespace Milestone\Appframe\Resolve;
 class ListResolver extends Resolve
 {
     public function yes(){
-        return empty($this->bag->req('update'));
+        return (
+            $this->bag->req('update') === null &&
+            $this->bag->req('get') === null &&
+            $this->bag->req('page') === null
+        );
     }
 
     public function idns(){
@@ -16,10 +20,12 @@ class ListResolver extends Resolve
         $idn1 = $this->bag->r('idns')['idn1'];
         $this->bag->r('list_id',$idn1);
         $this->bag->r('update',!empty($this->bag->req('update')));
+        $this->bag->r('page',$this->bag->req('page') ?: 1);
+        $this->bag->r('get',$this->bag->req('get'));
     }
 
     public function controllers(){
-        $Controllers = ['GetListController'];
+        $Controllers = [$this->bag->req('page') ? 'GetListPageItemsController' : 'GetListController'];
         if($this->yes()) array_unshift($Controllers,'GetListDetailsController');
         return array_map(function($controller){ return 'Milestone\\Appframe\\Controllers\\' . $controller; },$Controllers);
     }
