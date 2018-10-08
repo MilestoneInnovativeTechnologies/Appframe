af08badd6e4870bdff9be75b0e45d1da632f6fbe
Firose Hussain <github@firumon.com>
2018-10-09 02:06:10 +0530

    Controller to get ids from search term

---
+++ src/Controllers/GetListSearchItemsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Milestone\Appframe\Controllers;
+
+use Milestone\Appframe\Helper\Helper;
+
+class GetListSearchItemsController extends Controller
+{
+
+    public function index(){
+        $id = $this->bag->r('list_id'); $term = $this->bag->r('term');
+        $search = Helper::Help('GetListSearchWhere',$id,compact('term'));
+        $ORM = $this->getListORM($this->getListDetails($id),$search);
+        $items = $ORM->pluck('id');
+        $this->store($id,$term,$items);
+    }
+
+    private function getListDetails($id){
+        return $this->bag->session('List')[$id];
+    }
+
+    private function getListORM($Data,$Search){
+        $orm = $Data['orm']; $orm['Search'] = $Search; $orm['Take'] = 0;
+        return Helper::Help('GetOrm',$orm['Class'],array_except($orm,'Class'));
+    }
+
+    private function store($id, $term, $items){
+        $this->bag->store('ListSearchItems',$id,[ $term => $items ]);
+    }
+
+}
