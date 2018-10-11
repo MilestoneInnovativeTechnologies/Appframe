952ddb87c64b82d3e194a5e5789241136b27a63c
Firose Hussain <github@firumon.com>
2018-10-12 04:04:07 +0530

    Added special condition operators. AND Added a boolean property orm, when decided whether to output orm or orm executed data

--- src/Helper/GetOrmHelper.php
+++ src/Helper/GetOrmHelper.php
@@ -5,19 +5,19 @@ namespace Milestone\Appframe\Helper;
 class GetOrmHelper
 {
     private $class;
-    private $operator_string = ':operator', $items_per_page = 25;
-    public $With = [], $Scopes = [], $Where = [], $Search = [], $Page = 0, $Skip = 0, $Take = 25;
-    protected $orm;
+    private $operator_string = ':operator', $items_per_page = 25, $specialOperators = ['In','NotIn','like'];
+    public $orm = false, $With = [], $Scopes = [], $Where = [], $Search = [], $Page = 0, $Skip = 0, $Take = 25;
+    protected $ORM;
 
     public function __construct($class)
     {
         $this->class = $class;
-        $this->orm = new $class;
     }
 
     public function get(){
+        $this->ORM = ($this->orm) ? $this->class : new $this->class;
         $this->process();
-        return $this->orm;
+        return $this->ORM;
     }
 
     private function process(){
@@ -33,7 +33,7 @@ class GetOrmHelper
     private function processWith(){
         $with = $this->With;
         if($with && !empty($with))
-            $this->orm = $this->orm->with($with);
+            $this->ORM = $this->ORM->with($with);
     }
 
     private function processScopes(){
@@ -41,8 +41,8 @@ class GetOrmHelper
         if($scopes && is_array($scopes) && !empty($scopes) ){
             foreach ($scopes as $scope) {
                 $attrs = isset($scope[1]) ? $scope[1] : []; $method = $scope[0];
-                if ($attrs && !empty($attrs)) $this->orm = call_user_func_array([$this->orm, $method], $attrs);
-                else $this->orm = call_user_func([$this->orm, $method]);
+                if ($attrs && !empty($attrs)) $this->ORM = call_user_func_array([$this->ORM, $method], $attrs);
+                else $this->ORM = call_user_func([$this->ORM, $method]);
             }
         }
     }
@@ -54,15 +54,28 @@ class GetOrmHelper
                 if(mb_substr($field,-9) === $this->operator_string) continue;
                 $operator_key = $field . $this->operator_string;
                 $operator = array_key_exists($operator_key,$where) ? $where[$operator_key] : '=';
-                $this->orm = $this->orm->where($field,$operator,$value);
+                $this->ORM = (in_array($operator,$this->specialOperators)) ? $this->getSpecialWhereAppliedOrm($this->ORM,$field,$operator,$value) : $this->ORM->where($field,$operator,$value);
             }
         }
+    }
+
+    private function getSpecialWhereAppliedOrm($orm,$field,$operator,$value){
+        if($operator === 'like') return $this->getLikeWhereAppliedOrm($orm,$field,$value);
+        return $orm->{ 'where'.$operator }($field,(array) $value);
+    }
+
+    private function getLikeWhereAppliedOrm($orm,$field,$value){
+        return $orm->where($field,'like',$this->getLikeWrapTerm($value));
+    }
+
+    private function getLikeWrapTerm($term){
+        return implode('',['%',$term,'%']);
     }
 
     private function processSearch(){
         $search = $this->Search;
         if($search && is_array($search) && !empty($search) ){
-            $this->orm = $this->orm->where(function($Q) use($search){
+            $this->ORM = $this->ORM->where(function($Q) use($search){
                 $idx = 0;
                 foreach ($search as $field => $value) {
                     if(mb_substr($field,-9) === $this->operator_string) continue;
@@ -85,13 +98,13 @@ class GetOrmHelper
     private function processSkip(){
         $skip = $this->Skip;
         if($skip && $skip > 0)
-            $this->orm = $this->orm->skip($skip);
+            $this->ORM = $this->ORM->skip($skip);
     }
 
     private function processTake(){
         $take = $this->Take;
         if($take && $take > 0)
-            $this->orm = $this->orm->take($take);
+            $this->ORM = $this->ORM->take($take);
     }
 
 }
