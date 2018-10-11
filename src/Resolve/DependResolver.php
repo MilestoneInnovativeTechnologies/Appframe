02a7567a6b54e7d40f52a4e2a3a8231ee8475ced
Firose Hussain <github@firumon.com>
2018-10-12 01:08:27 +0530

    Resolver to handle Universal depend action created

---
+++ src/Resolve/DependResolver.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Milestone\Appframe\Resolve;
+
+class DependResolver extends Resolve
+{
+    public function yes(){
+        return false;
+    }
+
+    public function idns(){
+        return [];
+    }
+
+    public function prepare(){
+        $this->bag->r('field_id',$this->bag->req('form_field'));
+        $this->bag->r('field_name',$this->bag->req('field_name'));
+        $this->bag->r('data',$this->bag->req('data'));
+        $this->bag->r('form',$this->bag->req('form'));
+    }
+
+    public function controllers(){
+        $Controllers = ['GetDependFieldDataController'];
+        return $this->namespacedControllers($Controllers);
+    }
+
+    private function namespacedControllers($controllersArray){
+        return array_map(function($controller){ return 'Milestone\\Appframe\\Controllers\\' . $controller; },$controllersArray);
+    }
+}
