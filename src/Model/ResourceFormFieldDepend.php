44b4b86bf03e2722240a6b03bd9f3a9198ec2d7a
Firose Hussain <github@firumon.com>
2018-10-11 18:58:06 +0530

    All fields from this table outputs

--- src/Model/ResourceFormFieldDepend.php
+++ src/Model/ResourceFormFieldDepend.php
@@ -5,5 +5,4 @@ namespace Milestone\Appframe\Model;
 class ResourceFormFieldDepend extends Model
 {
     protected $table = '__resource_form_field_depends';
-    protected $visible = ['depend_field'];
 }
