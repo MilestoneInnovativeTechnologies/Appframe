5c85f5ec9885d5624db389daf9521cb54e7202fa
Firose Hussain <github@firumon.com>
2018-10-17 00:39:39 +0530

    Metric Table Modified

---
+++ migrations/2018_10_16_184721_create___resource_metrics_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResourceMetricsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('__resource_metrics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('resource')->index();
+            $table->string('name', 64)->index();
+            $table->unsignedInteger('resource_list')->index();
+            $table->enum('aggregates', ['COUNT','SUM','AVG','MAX','MIN'])->default('COUNT');
+            $table->string('aggregate_field', 64)->nullable();
+            $table->enum('aggregate_distinct', ['No','Yes'])->default('No');
+            $table->string('field', 64)->nullable();
+            $table->string('field_sub', 64)->nullable();
+            $table->unsignedTinyInteger('cache')->default(0);
+            $table->string('method', 128)->nullable();
+            $table->timestamps();
+            $table->foreign('resource')->references('id')->on('__resources')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('resource_list')->references('id')->on('__resource_lists')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('__resource_metrics');
+    }
+}
