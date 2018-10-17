19a8e2de6e37e5e1048b2943d390399bfedd18b3
Firose Hussain <github@firumon.com>
2018-10-17 21:12:56 +0530

    New resolve for dashboard created

--- src/Resolve/DashboardResolver.php
+++ src/Resolve/DashboardResolver.php
@@ -26,6 +26,7 @@ class DashboardResolver extends Resolve
     public function controllers(){
         $Controllers = ($this->yes()) ? ['GetDashboardController'] : [];
         if($this->bag->r('list_id')) if($this->bag->req('item_action') == 'fetch') array_push($Controllers,'GetListDetailsController','GetListController'); else array_push($Controllers,'GetListController');
+        if($this->bag->r('metric_id')) if($this->bag->req('item_action') == 'fetch') array_push($Controllers,'GetMetricDetailsController','GetMetricController'); else array_push($Controllers,'GetMetricController');
         return array_map(function($controller){ return 'Milestone\\Appframe\\Controllers\\' . $controller; },$Controllers);
     }
 
@@ -34,6 +35,13 @@ class DashboardResolver extends Resolve
         $item_id = $this->bag->req('item_id');
         if($items->search(function($item)use($item_id){ return $item['item'] == 'List' && $item['item_id'] == $item_id; }) === false) return;
         $this->bag->r('list_id',$item_id);
+    }
+
+    private function prepareDashboardMetricItem(){
+        $items = Helper::Help('GetDashboardItems',$this->bag->r('dashboard_id'));
+        $item_id = $this->bag->req('item_id');
+        if($items->search(function($item)use($item_id){ return $item['item'] == 'Metric' && $item['item_id'] == $item_id; }) === false) return;
+        $this->bag->r('metric_id',$item_id);
     }
 
 }
