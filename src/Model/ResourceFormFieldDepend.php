75f3e1f22cab43b92f6cec6a98550ed5838c2eea
Firose Hussain <github@firumon.com>
2018-11-03 07:43:27 +0530

    Added relation field

--- src/Model/ResourceFormFieldDepend.php
+++ src/Model/ResourceFormFieldDepend.php
@@ -5,4 +5,8 @@ namespace Milestone\Appframe\Model;
 class ResourceFormFieldDepend extends Model
 {
     protected $table = '__resource_form_field_depends';
+
+    public function Field(){
+        return $this->belongsTo(ResourceFormField::class, 'form_field', 'id');
+    }
 }
