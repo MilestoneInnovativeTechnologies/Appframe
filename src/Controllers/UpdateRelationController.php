689e45b3f8307c4260132d3598e935f8bf9b1e6f
Firose Hussain <github@firumon.com>
2018-11-07 15:03:39 +0530

    Changes applied to make the controller working

--- src/Controllers/UpdateRelationController.php
+++ src/Controllers/UpdateRelationController.php
@@ -10,9 +10,14 @@ class UpdateRelationController extends Controller
     public function index(){
         $relation_id = $this->bag->r('relation_id'); $relations = $this->bag->r('relations'); $record_id = $this->bag->r('record_id');
         $Relation = ResourceRelation::with(['Owner','Resource'])->find($relation_id);
-        $RelationExtract = (new Database\RelationExtract($Relation))->process($record_id,$relations);
-        $Submit = new Database\Push($RelationExtract->relationGrouped->toArray(),$RelationExtract->resourceModel);
+        $Model = $this->getResourceModel($Relation->Owner,$record_id); $Method = $Relation->method;
+        $Submit = $Model->$Method()->sync($relations);
         $this->bag->keep('Submit',$Submit);
+    }
+
+    private function getResourceModel($resource,$id = null){
+        $Class = implode('\\',[$resource->namespace,$resource->name]);
+        return ($id) ? (new $Class)->find($id) : new $Class;
     }
 
 
