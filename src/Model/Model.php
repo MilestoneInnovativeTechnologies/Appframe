6f20756a8941283ae9d717dfd768db6f51b6ef3e
Firose Hussain <github@firumon.com>
2018-11-08 19:24:20 +0530

    upload files field defined, then automatic appends will be added for getting upload details

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model as BaseModel;
 
 class Model extends BaseModel
 {
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        if($this->files && is_array($this->files) && !empty($this->files)){
+            $this->setAppends(['__upload_file_details']);
+        }
+    }
 
+    public function getUploadFileDetailsAttribute(){
+        $files = [];
+        foreach($this->files as $field){
+            if($this->$field) $files[$field] = ResourceFormUpload::where('code',$this->$field)->first();
+        }
+        return $files;
+    }
 }
