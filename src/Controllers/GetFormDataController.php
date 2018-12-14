8054001dffdf489d4cb519349154756538e2ad10
Firose Hussain <github@firumon.com>
2018-12-14 11:43:17 +0530

    Helper made use

--- src/Controllers/GetFormDataController.php
+++ src/Controllers/GetFormDataController.php
@@ -2,20 +2,16 @@
 
 namespace Milestone\Appframe\Controllers;
 
-use Milestone\Appframe\Model\Resource;
-use Milestone\Appframe\Model\ResourceData;
+use Milestone\Appframe\Helper\Helper;
 
 class GetFormDataController extends Controller
 {
 
     public function index(){
-        $DataId = $this->bag->r('data_id'); $RecId = $this->bag->r('record');
-        $ResData = ResourceData::find($DataId);
-        $Res = Resource::find($ResData->resource);
-        $Class = implode('\\',[$Res->namespace,$Res->name]);
-        $updatedAt = $this->bag->req('last_updated') ?: 0;
-        $Data = (new $Class)->where('updated_at','>',$updatedAt)->find($RecId);
-        $this->bag->store('Data',$DataId,$Data); $this->bag->store('FormData',$this->bag->r('form_id'),[$DataId => $RecId]);
+        $DataId = $this->bag->r('data_id'); $record_id = $this->bag->r('record');
+        $last_updated = $this->bag->req('last_updated') ?: 0;
+        $Data = Helper::Help('GetData',$DataId,compact('record_id','last_updated'));
+        $this->bag->store('Data',$DataId,$Data); $this->bag->store('FormData',$this->bag->r('form_id'),[$DataId => $record_id]);
     }
 
 }
