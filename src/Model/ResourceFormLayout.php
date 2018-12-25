e70a7f08afa793c63e51004da9ccb142913e3286
Firose Hussain <github@firumon.com>
2018-12-25 16:39:11 +0530

    Form and Field Relations added

--- src/Model/ResourceFormLayout.php
+++ src/Model/ResourceFormLayout.php
@@ -5,4 +5,12 @@ namespace Milestone\Appframe\Model;
 class ResourceFormLayout extends Model
 {
     protected $table = '__resource_form_layout';
+
+    public function Form(){
+        return $this->belongsTo(ResourceForm::class,'resource_form', 'id');
+    }
+
+    public function Field(){
+        return $this->belongsTo(ResourceFormField::class,'form_field', 'id');
+    }
 }
