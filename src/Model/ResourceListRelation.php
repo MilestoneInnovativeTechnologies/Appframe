6a310a25c60457e984aa420fca2a02c7ea476529
Firose Hussain <github@firumon.com>
2018-12-26 16:36:01 +0530

    Added relations

--- src/Model/ResourceListRelation.php
+++ src/Model/ResourceListRelation.php
@@ -5,4 +5,16 @@ namespace Milestone\Appframe\Model;
 class ResourceListRelation extends Model
 {
     protected $table = '__resource_list_relations';
+
+    public function List(){
+        return $this->belongsTo(ResourceList::class,'resource_list','id');
+    }
+
+    public function Relation(){
+        return $this->belongsTo(ResourceRelation::class,'relation','id');
+    }
+
+    public function NRelation(){
+        return $this->belongsTo(ResourceRelation::class,'nest_relation1','id');
+    }
 }
