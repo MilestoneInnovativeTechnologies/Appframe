ae34ffb4828cdc70f64bcac83bb78f5c7e3d3cd7
Firose Hussain <github@firumon.com>
2018-12-26 16:46:26 +0530

    Added relations

--- src/Model/ResourceListScope.php
+++ src/Model/ResourceListScope.php
@@ -5,4 +5,12 @@ namespace Milestone\Appframe\Model;
 class ResourceListScope extends Model
 {
     protected $table = '__resource_list_scopes';
+
+    public function List(){
+        return $this->belongsTo(ResourceList::class,'resource_list','id');
+    }
+
+    public function Scope(){
+        return $this->belongsTo(ResourceScope::class,'scope','id');
+    }
 }
