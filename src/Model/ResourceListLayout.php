92426817c039d8dc30881b7e4610f422ecb95da1
Firose Hussain <github@firumon.com>
2018-12-26 16:48:38 +0530

    Added relations

--- src/Model/ResourceListLayout.php
+++ src/Model/ResourceListLayout.php
@@ -5,4 +5,8 @@ namespace Milestone\Appframe\Model;
 class ResourceListLayout extends Model
 {
     protected $table = '__resource_list_layout';
+
+    public function List(){
+        return $this->belongsTo(ResourceList::class,'resource_list','id');
+    }
 }
