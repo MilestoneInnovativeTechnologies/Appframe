0ed57c3bb6722450cf6cd3bffb700d788bfcb10b
Firose Hussain <github@firumon.com>
2019-01-28 11:34:13 +0530

    Resource Defaults seed data added

--- src/Seeder/ResourceDefaultTableSeeder.php
+++ src/Seeder/ResourceDefaultTableSeeder.php
@@ -16,6 +16,7 @@ class ResourceDefaultTableSeeder extends Seeder
         $_ = \DB::statement('SELECT @@GLOBAL.foreign_key_checks');
         \DB::statement('set foreign_key_checks = 0');
         \Milestone\Appframe\Model\ResourceDefault::truncate()
+            ->create([	'id' => '1', 	'resource' => '4', 	'list' => '20', 	'create' => '5', 												])
         ;
         \DB::statement('set foreign_key_checks = ' . $_);
     }
