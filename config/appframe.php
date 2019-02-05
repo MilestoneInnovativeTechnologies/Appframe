32c81ab3a95b10f44d5c30e66207dd53b625aec3
Firose Hussain <github@firumon.com>
2019-02-05 11:27:57 +0530

    Index Route and Logged Out Route added for redirecting to custom routes

--- config/appframe.php
+++ config/appframe.php
@@ -74,4 +74,30 @@ return [
     */
 
     'root_path' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Index Route
+    |--------------------------------------------------------------------------
+    |
+    | The route name to redirect to show index page/welcome page.
+    | If no any route name mention, default Appframe root route
+    | will be used
+    |
+    */
+
+    'index_route' => 'root',
+
+    /*
+    |--------------------------------------------------------------------------
+    | LoggedOut Route
+    |--------------------------------------------------------------------------
+    |
+    | The route name to redirect to after logging out.
+    | If no any route name mention, default Appframe login route
+    | will be used
+    |
+    */
+
+    'logged_out_route' => 'login',
 ];
