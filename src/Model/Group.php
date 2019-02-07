934412336cd842510efd65ddc89d680050203eaa
Firose Hussain <github@firumon.com>
2019-02-07 22:01:10 +0530

    Global scope added to avoid display of protected groups

--- src/Model/Group.php
+++ src/Model/Group.php
@@ -2,10 +2,17 @@
 
 namespace Milestone\Appframe\Model;
 
+use Illuminate\Database\Eloquent\Builder;
+
 class Group extends Model
 {
     protected $table = '__groups';
-
+    protected static function boot(){
+        parent::boot();
+        static::addGlobalScope('withoutSetup', function (Builder $builder) {
+            $builder->where(function($Q){ $Q->whereNotIn('name',['setup_user','developers']); });
+        });
+    }
     public function Users(){
         return $this->belongsToMany(User::class, '__group_users', 'group', 'role')->withTimestamps();
     }
