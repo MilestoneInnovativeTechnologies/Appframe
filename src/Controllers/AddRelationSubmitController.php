dd42ec190e71f7d3bc7ab26b4ba6c58abc73a7fd
Firose Hussain <github@firumon.com>
2019-02-20 21:48:58 +0530

    Handles IDN3 for AddRelation method

--- src/Controllers/AddRelationSubmitController.php
+++ src/Controllers/AddRelationSubmitController.php
@@ -3,7 +3,7 @@
 namespace Milestone\Appframe\Controllers;
 
 use Milestone\Appframe\Helper\Helper;
-use Milestone\Appframe\Model\ResourceForm;
+use Milestone\Appframe\Model\ResourceRelation;
 
 class AddRelationSubmitController extends Controller
 {
@@ -15,8 +15,9 @@ class AddRelationSubmitController extends Controller
     }
 
     private function SubmitForm(){
-        $form = $this->bag->r('form_id'); $record = $this->bag->r('record'); $input = $this->bag->r('data');
+        $form = $this->bag->r('form_id'); $relation_id = $this->bag->r('relation_id'); $record = $this->bag->r('record'); $input = $this->bag->r('data');
         $relation_data = Helper::Help('FormSubmitRelation',$form,compact('input'));
+        if(!$this->bag->r('field_id')) $relation_data = $this->wrapRelation($relation_data,$relation_id,$record);
         $this->bag->r('submit_relations',$this->getRelations($relation_data));
         $response = (new Database\Bind($relation_data))->store();
         $this->bag->store('FormSubmit',$form,!!$response); $this->bag->r('submit_model',$response[0]);
@@ -25,5 +26,27 @@ class AddRelationSubmitController extends Controller
     private function getRelations($relation_data){
         $relations = array_column($relation_data[0]['records'],'relations');
         return (empty($relations) || !isset($relations[0]) || empty($relations[0])) ? [] : array_map(function($relation){ return $relation[0]['method']; },$relations);
+    }
+
+    private function wrapRelation($relations,$relation_id,$id){
+        $relation = ResourceRelation::with(['Owner'])->find($relation_id);
+        $set = $this->getDefaultPropertySet();
+        $set['id'] = $id; $set['class'] = implode("\\",[$relation->Owner->namespace,$relation->Owner->name]);
+        $relations[0]['type'] = $relation->type; $relations[0]['method'] = $relation->method;
+        $set['records'][0]['relations'] = $relations;
+        return [$set];
+    }
+
+    private function getDefaultPropertySet(){
+        $set = array_fill_keys([
+            'id','class','type','method','records'
+        ],null);
+        $set['records'] = [
+            [
+                'data' => [],
+                'relations' => []
+            ]
+        ];
+        return $set;
     }
 }
