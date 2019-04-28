1642d5fc8ab5530fbdd8bc1e8e12eefa64ddbc5d
Firose Hussain <github@firumon.com>
2019-04-28 15:59:52 +0530

    Eloquent relations hasOneThrough and hasManyThrough have added

--- migrations/2019_01_24_000008_create___resource_relations_table.php
+++ migrations/2019_01_24_000008_create___resource_relations_table.php
@@ -16,10 +16,10 @@ class CreateResourceRelationsTable extends Migration
         Schema::create('__resource_relations', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('resource')->index();
-            $table->string('name', 64)->index();
-            $table->string('description', 1024)->nullable();
-            $table->string('method', 128)->nullable();
-            $table->enum('type', ['hasOne','hasMany','belongsTo','belongsToMany'])->default('hasMany');
+            $table->string('name', '64')->index();
+            $table->string('description', '1024')->nullable();
+            $table->string('method', '128')->nullable();
+            $table->enum('type', ['hasOne','hasMany','belongsTo','belongsToMany','hasOneThrough','hasManyThrough'])->default('hasMany');
             $table->unsignedInteger('relate_resource')->index()->nullable();
             $table->audit();
             $table->foreign('resource')->references('id')->on('__resources')->onUpdate('cascade')->onDelete('cascade');
