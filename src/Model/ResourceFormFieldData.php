7c2252f0f8c40f964c5c1e3976bb18fc4ad6a4ca
Firose <firumon@github.com>
2019-12-10 09:47:28 +0530

    Form field data wont insert for empty attribute record. Solved Issue #12

--- src/Model/ResourceFormFieldData.php
+++ src/Model/ResourceFormFieldData.php
@@ -4,6 +4,11 @@ namespace Milestone\Appframe\Model;
 
 class ResourceFormFieldData extends Model
 {
+    protected static function boot(){
+        parent::boot();
+        static::creating(function($model){ if(!$model->attribute) return false; });
+    }
+
     protected $table = '__resource_form_field_data';
     protected $with = ['Relation'];
 
